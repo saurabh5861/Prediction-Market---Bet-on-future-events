f7e4d9d9a95cbe9d96affbdb149d732adac2d57f
saurabh5861 <sourabhsuman5861@gmail.com>
2025-05-22 13:58:12 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,36 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
-    coreTestnet: {
+    core_testnet2: {
       url: "https://rpc.test2.btcs.network",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 1114,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1115,
+      gasPrice: 20000000000,
     },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    }
   },
   paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
     sources: "./contracts",
     tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
   },
+  mocha: {
+    timeout: 40000
+  }
 };
