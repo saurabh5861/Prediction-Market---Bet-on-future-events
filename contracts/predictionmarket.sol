d1d1852149ea42e99248a0083f9a9f4f1ed4682a
saurabh5861 <sourabhsuman5861@gmail.com>
2025-08-22 23:50:22 +0530

    Update predictionmarket.sol

--- contracts/predictionmarket.sol
+++ contracts/predictionmarket.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: 
+// SPDX-License-Identifier: mit
 pragma solidity ^0.8.19;
 contract CarbonCreditMarketplace {
     struct CarbonCredit {
@@ -256,6 +256,7 @@ contract CarbonCreditMarketplace {
         owner = _newOwner;
     }
 }
+
 
 
 
